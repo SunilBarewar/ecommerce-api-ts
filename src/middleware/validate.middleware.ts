0eb3b9455fcc8d5f152257b644f618a8853868c4
sunil barewar <sunilvbarewar@gmail.com>
2024-01-08 00:22:10 +0530

    validate request

---
+++ src/middleware/validate.middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+const validate =
+  (schema: z.AnyZodObject | z.ZodOptional<z.AnyZodObject>) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+
+      next();
+    } catch (error) {
+      let err = error;
+      if (err instanceof z.ZodError) {
+        err = err.issues.map((e) => ({ path: e.path[0], message: e.message }));
+      }
+      return res.status(409).json({
+        status: "failed",
+        error: err,
+      });
+    }
+  };
+
+export default validate;
