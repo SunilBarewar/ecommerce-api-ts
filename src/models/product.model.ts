fc197f31fa5af0e885ef72993f0525a1d6652bfb
sunil barewar <sunilvbarewar@gmail.com>
2024-01-10 23:15:01 +0530

    refactor: product schema

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,27 +1,30 @@
 import { Schema, model } from "mongoose";
+import { string } from "zod";
 
 interface IProduct {
-  name: string;
-  desc: string;
+  title: string;
+  description: string;
   price: number;
   stock: number;
   rating: number;
   category: string;
-  discount: number;
+  discountPercentage: number;
   thumbnail: string;
   images: string[];
+  brand: string;
 }
 
 const productSchema = new Schema<IProduct>({
-  name: { type: String, required: true, index: true },
-  desc: { type: String, required: true },
+  title: { type: String, required: true, index: true },
+  description: { type: String, required: true },
   price: { type: Number, required: true, index: true },
   rating: { type: Number, required: true, index: true },
   stock: { type: Number, required: true },
   category: { type: String, required: true },
-  discount: { type: Number, required: true },
+  discountPercentage: { type: Number, required: true },
   thumbnail: { type: String, required: true },
   images: { type: [String], required: true },
+  brand: { type: String, required: true },
 });
 
 const ProductModel = model<IProduct>("product", productSchema);
--- src/schemas/product.schema.ts
+++ src/schemas/product.schema.ts
@@ -1,15 +1,16 @@
 import { z } from "zod";
 
 const body = z.object({
-  name: z.string().min(1, "Name cannot be empty"),
-  desc: z.string().min(10, "Name cannot be empty"),
+  title: z.string().min(1, "Name cannot be empty"),
+  description: z.string().min(10, "Name cannot be empty"),
   price: z.number().min(1, "price cannot be zero"),
   stock: z.number(),
   category: z.string(),
-  discount: z.number(),
+  discountPercentage: z.number(),
   thumbnail: z.string(),
   images: z.array(z.string()),
   rating: z.number(),
+  brand: z.string(),
 });
 
 const params = z.object({
@@ -21,12 +22,12 @@ const query = z.object({
   category: z.string().optional(),
   search: z.string().optional(),
   sortBy: z.string().optional(),
-  sortOrder: z.enum(['asc', 'desc']).optional(),
+  sortOrder: z.enum(["asc", "desc"]).optional(),
 });
 
 export const FilterProductSchema = z.object({
-  query
-})
+  query,
+});
 
 export const CreateProductSchema = z.object({
   body,
@@ -50,4 +51,4 @@ export type CreateProductDto = z.infer<typeof CreateProductSchema>;
 export type UpdateProductDto = z.infer<typeof UpdateProductSchema>;
 export type DeleteProductDto = z.infer<typeof DeleteProductSchema>;
 export type GetProductDto = z.infer<typeof GetProductSchema>;
-export type FilterProductsDto = z.infer<typeof FilterProductSchema>
+export type FilterProductsDto = z.infer<typeof FilterProductSchema>;
