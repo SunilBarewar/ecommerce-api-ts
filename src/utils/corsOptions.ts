f6a24eda90a64b60c4bde4d2ca602ac8a701d399
sunil barewar <sunilvbarewar@gmail.com>
2024-01-14 00:22:55 +0530

    changed cors options

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -1,5 +1,17 @@
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://e-commerce-frontend-sunilbarewar.vercel.app",
+  "https://e-commerce-frontend-iota-nine.vercel.app",
+];
+
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: (origin: any, callback: Function) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   optionsSuccessStatus: 200,
 };
