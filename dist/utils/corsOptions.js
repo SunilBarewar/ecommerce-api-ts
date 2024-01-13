c43d36866655d062db6aae3c0836cab90c27784f
sunil barewar <sunilvbarewar@gmail.com>
2024-01-13 23:46:04 +0530

    removed dist

--- dist/utils/corsOptions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true,
-    optionsSuccessStatus: 200,
-};
-exports.default = corsOptions;
--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL,
   credentials: true,
   optionsSuccessStatus: 200,
 };
